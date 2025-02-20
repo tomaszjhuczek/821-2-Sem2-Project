27f4bfabc5e325bb8070c693a8ccd0fa31700772
Tomasz J. Huczek <tomasz@huczek.ie>
2025-02-20 12:00:46 +0000

    Add Crop Class

---
+++ Crop.cpp
@@ -0,0 +1,23 @@
+//
+// Created by tomasz on 13/02/25.
+//
+
+#include "Crop.h"
+
+Crop::Crop() {
+    
+}
+
+Crop::~Crop() {
+    
+}
+
+
+
+string Crop::getName() {
+    return this->name;
+}
+
+void Crop::setName(string name) {
+    this->name = name;
+}
